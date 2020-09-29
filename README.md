4e907d54f66adbe8e35e111623c4ca10ea60c329
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-09-29 12:36:25 +0200

    Simple Image Gallery

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image Gallery
+
+Simple Image Gallery using images from [Lorem Picsum](https://picsum.photo).
---
+++ script.js
@@ -0,0 +1,37 @@
+(() => {
+  const gallery = document.querySelector('.gallery');
+  const imageView = document.querySelector('.image-view');
+  const images = document.createDocumentFragment();
+
+  imageView.addEventListener('click', closeImageView);
+
+  for (let i = 1; i <= 24; i++) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.setAttribute('src', `https://picsum.photos/seed/${i}/300`);
+    img.setAttribute('alt', `Image ${i}`);
+    img.dataset.seed = i;
+    img.classList.add('image');
+    img.addEventListener('click', openImageView);
+    div.classList.add('image-container');
+    div.appendChild(img);
+    images.appendChild(div);
+  }
+  
+  gallery.insertBefore(images, gallery.children[1]);
+
+  function openImageView(e) {
+    const { alt, dataset: { seed } } = e.target;
+    const img = document.createElement('img');
+    img.setAttribute('src', `https://picsum.photos/seed/${seed}/800`);
+    img.setAttribute('alt', alt);
+    img.classList.add('image-view-image');
+    imageView.appendChild(img);
+    imageView.classList.add('image-view-active');
+  }
+  
+  function closeImageView() {
+    imageView.classList.remove('image-view-active');
+    imageView.textContent = '';
+  }
+})(); 
